26d05caf9edd45977f7b7de54d4708d22f85976c
github_Jeptooleonida <leonida.jeptoo@student.moringaschool.comgit config --global user.name Jeptooleonidagit config --global user.email leonida.jeptoo@student.moringaschool.com>
2022-08-22 02:12:42 +0300

    Done

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,25 @@
 #write your code here
 
-def countdown
-end
+def countdown num
+
+    x = 1
+    while x < num + 1
+      puts "#{(num + 1) - x} SECOND(S)!"
+  
+      x += 1
+  
+    end
+  
+    return "HAPPY NEW YEAR!"
+  end
+  
+  def countdown_with_sleep num
+    x = 1
+    while x < num + 1
+      puts "#{(num + 1) - x} SECOND(S)!"
+      x += 1
+      sleep(1)
+    end
+    return "Happy New YEAR!"
+  end
+  countdown 5
